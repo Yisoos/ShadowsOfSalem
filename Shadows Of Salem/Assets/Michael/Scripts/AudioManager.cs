c57b16aecc41f0686c49b1cdcf63da50aeb46bb9
MichaaelCR <miichaaelcr@gmail.com>
2024-10-24 10:20:56 +0200

    Script AudioManager
    Music and SFX Source

---
+++ Shadows Of Salem/Assets/Michael/Scripts/AudioManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("---------- Audio Source ----------")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("---------- Audio Clip ----------")]
+    public AudioClip background;
+    public AudioClip sound1;
+    public AudioClip sound2;
+    public AudioClip sound3;
+    public AudioClip sound4;
+    public AudioClip sound5;
+
+
+    private void Start()
+    {
+         musicSource.clip = background;
+         musicSource.Play();
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+    }
+}
