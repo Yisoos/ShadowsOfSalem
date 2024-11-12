4526c5aaf9ff1c868d128392419bae0a7e0146aa
Yisoos <155551241+Yisoos@users.noreply.github.com>
2024-11-12 23:56:03 +0100

    Update RotaryDial.cs

--- Shadows Of Salem/Assets/Scripts/ItemInteraction/Objects/RotaryDial.cs
+++ Shadows Of Salem/Assets/Scripts/ItemInteraction/Objects/RotaryDial.cs
@@ -7,29 +7,53 @@ public class RotaryDial : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDr
     public float StartingRotation;
     public float EndRotation;
     [Range(0,60)]public float dialReturnSpeed;
+    private Vector2 Current;
+    private float previousAngle; 
+    private float currentAngle;
+    private float startRotation;
 
-    private Vector2 Current ;
     private void Start()
     {
-        
+        startRotation = transform.rotation.eulerAngles.z;
+    }
+
+    float GetAngleBetweenPoints(Vector3 from, Vector3 to)
+    {
+        // Calculate direction vector from 'from' to 'to'
+        Vector3 direction = to - from;
+
+        // Get the currentAngle in radians, then convert to degrees
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        // Make sure currentAngle is always positive (0 to 360 degrees)
+        if (angle < 0) angle += 360f;
+
+        return angle;
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-       
+        Vector3 origin = transform.position;
+        Vector3 pointer = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+        previousAngle = GetAngleBetweenPoints(origin, pointer); ;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        
+        Vector3 origin = transform.position;
+        Vector3 pointer = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+        currentAngle = GetAngleBetweenPoints(origin, pointer);
+        Debug.Log("Angle between points: " + currentAngle + " degrees");
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z+(currentAngle - previousAngle));
+        previousAngle = GetAngleBetweenPoints(origin, pointer); ;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        
+        StartCoroutine(ReturnDialPosition());
     }
 
-    private IEnumerator ReturnToOriginalPositionCounterClockwise()
+    private IEnumerator ReturnDialPosition()
     {
         return null;
     }
