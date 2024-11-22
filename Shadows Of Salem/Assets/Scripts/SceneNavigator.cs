37c4ab4beccbee3f1fc3db872cf6b1a5edec151b
Yisoos <155551241+Yisoos@users.noreply.github.com>
2024-11-22 18:15:37 +0100

    Update SceneNavigator.cs

--- Shadows Of Salem/Assets/Scripts/SceneNavigator.cs
+++ Shadows Of Salem/Assets/Scripts/SceneNavigator.cs
@@ -6,11 +6,12 @@ using UnityEngine;
 
 public class SceneNavigator : MonoBehaviour
 {
-    [Header ("Views")]
-    [Tooltip("Arranstrar aqui todas las vistas generales que se van a cambiar con las flechas")]public GameObject[] view; //Las diferentes vistas guardadas en el inspector
+    [Header("Views")]
+    [Tooltip("Arranstrar aqui todas las vistas generales que se van a cambiar con las flechas")] public GameObject[] view; //Las diferentes vistas guardadas en el inspector
     [Space(20)] public int startingView; //la vista donde empezará la escena
     [Space(20), Min(0)] public int[] roomEndViews;
-    [HideInInspector] public int currentView; //la escena actual
+    /*[HideInInspector]*/
+    public int currentView; //la escena actual
     [Space(20)] int maxViews;
     [Header("Arrows")]
     public GameObject[] arrow; //flechas en la interfaz que se usan para moverse
@@ -19,10 +20,10 @@ public class SceneNavigator : MonoBehaviour
     void Start()
     {
         currentView = startingView;
-        maxViews = view.Length-1;
-        for (int i = 0; i < view.Length; i++) 
+        maxViews = view.Length - 1;
+        for (int i = 0; i < view.Length; i++)
         {
-            if ((currentView==i)) 
+            if ((currentView == i))
             {
                 view[i].SetActive(true);
             }
@@ -36,29 +37,31 @@ public class SceneNavigator : MonoBehaviour
         //Debug.Log($"Max view:{maxViews}");
         CheckViewEnd();
     }
-    public void CheckViewEnd() 
+    public void CheckViewEnd()
     {
-        if(roomEndViews != null) 
+        if (roomEndViews != null)
         {
             for (int i = 0; i < roomEndViews.Length; i++)
             {
-                if (currentView == 0 || currentView == roomEndViews[i]+1)  // Is la vista actual esta en el borde izquierdo
+                if (currentView == 0 || currentView == roomEndViews[i] + 1)  // Is la vista actual esta en el borde izquierdo
                 {
                     arrow[0].SetActive(false);  // desactiva la flecha izquierda
                     arrow[1].SetActive(true);   // Activa la flecha derecha
-                                                //Debug.Log("Left end reached");
+                    break;
+                    //Debug.Log("Left end reached");
                 }
                 else if (currentView == maxViews || currentView == roomEndViews[i])  // Si la vista actual esta en el borde derecho
                 {
                     arrow[0].SetActive(true);    // Activa la flecha izquierda
                     arrow[1].SetActive(false);   // Activa la flecha derecha
-                                                 //Debug.Log("Right end reached");
+                    break;
+                    //Debug.Log("Right end reached");
                 }
                 else  // Si la vista actual no esta a los bordes
                 {
                     arrow[0].SetActive(true);    // Activa la flecha izquierda
                     arrow[1].SetActive(true);    // Activa la flecha derecha
-                                                 // Debug.Log("Not at the edge views");
+                    // Debug.Log("Not at the edge views");
                 }
             }
         }
@@ -69,17 +72,17 @@ public class SceneNavigator : MonoBehaviour
         {
             view[newView].SetActive(true);
             view[currentView].SetActive(false);
-            currentView=newView;
+            currentView = newView;
             CheckViewEnd();
             //Debug.Log($"current view is {currentView}");
         }
     }
 
-    public void ChangeViewRight() 
+    public void ChangeViewRight()
     {
-       if(currentView != maxViews) //si la vista actual no es el limite a la derecha, activa la escena a la derecha
+        if (currentView != maxViews) //si la vista actual no es el limite a la derecha, activa la escena a la derecha
         {
-            view[currentView+1].SetActive(true);
+            view[currentView + 1].SetActive(true);
             view[currentView].SetActive(false);
             currentView++;
             CheckViewEnd();
@@ -94,7 +97,7 @@ public class SceneNavigator : MonoBehaviour
             view[currentView].SetActive(false);
             currentView--;
             CheckViewEnd();
-           // Debug.Log($"current view is {currentView}");
+            // Debug.Log($"current view is {currentView}");
         }
     }
     public void EnterCloseUpViewArrows()
