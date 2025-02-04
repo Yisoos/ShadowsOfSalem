478d5d31552fa6b59dbe1ba9e078931758482608
Yisoos <155551241+Yisoos@users.noreply.github.com>
2025-02-04 22:47:22 +0100

    Squashed commit of the following:
    commit 2891815ae0c0d7181683e56e348aa62a6a315469
    Author: dianaburguillo <diana.burguillo@estudiante.epsum.school>
    Date:   Tue Feb 4 11:56:34 2025 +0100
    
        ultimo
    
    commit da6797cfb3566ad775fa1b321a856433c157f9e8
    Author: dianaburguillo <diana.burguillo@estudiante.epsum.school>
    Date:   Tue Feb 4 11:47:38 2025 +0100
    
        a que precio
    
    commit 71ce0cac813153df2a0b0c2b7bed51757f4adbef
    Author: dianaburguillo <diana.burguillo@estudiante.epsum.school>
    Date:   Tue Feb 4 02:40:23 2025 +0100
    
        ups

---
+++ Shadows Of Salem/Assets/Scripts/CajaFuerte#auxilio/SafeCombinationController.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SafeCombinationController : MonoBehaviour
+{
+
+    public int[] correctCombination = { 14, 6, 11 }; // Contraseña correcta (14-06-11)
+    private int currentIndex = 0; // Estado inicial de la contraseña
+    public FeedbackTextController feedbackText; // Referencia al FeedbackTextController
+    public GameObject closeUpObject; // Referencia al objeto de close up
+    public bool isLocked = true;
+
+
+    private void OnMouseDown()
+    {
+        if ( (isLocked))
+        {
+            closeUpObject.SetActive(true);
+        }
+    }
+    // verifica si el número introducido es correcto
+    public void CheckNumber(int number)
+    {
+
+        // Si el número es correcto
+        if (number == correctCombination[currentIndex])
+        {
+            currentIndex++; // Avanza al siguiente número de la contraseña
+
+            feedbackText.PopUpText("Código: " + GetCurrentCode()); // Muestra el código introducido
+
+
+            // Si se ha introducido toda la contraseña, desbloquea la caja fuerte
+            if (currentIndex >= correctCombination.Length)
+            {
+                UnlockSafe();
+            }
+        }
+
+        else // Si el número es incorrecto, reinicia la secuencia
+        {
+            
+            currentIndex = 0;
+            feedbackText.PopUpText("Código incorrecto. Inténtalo de nuevo.");
+        }
+
+    }
+
+
+
+    // Método para obtener el código introducido correctamente hasta el momento
+    private string GetCurrentCode()
+    {
+        string code = "";
+
+        for (int i = 0; i < currentIndex; i++)
+        {
+            code += correctCombination[i].ToString("00") + " "; 
+        }
+        return code.Trim(); // Elimina el espacio final
+    }
+
+
+
+    // desbloquear la caja fuerte
+    private void UnlockSafe()
+    {
+        feedbackText.PopUpText("Caja fuerte desbloqueada.");
+        isLocked = false;
+        // Cerrar el close up
+        if (closeUpObject != null)
+        {
+            closeUpObject.SetActive(false); 
+        }
+     }
+
+}
+
+
